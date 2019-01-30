16668a0dfa63f87faff83d10a8cc9a1f83678c91
Jonnathan Campoberde <jonnathanca9601@gmail.com>
2019-01-30 15:45:16 -0500

    Definicion de la base de datos lista

--- sequelize.js
+++ sequelize.js
@@ -1,10 +1,21 @@
 var Sequelize = require('sequelize');
+const ComentariosModel = require('./models/Comentarios');
+const ComentServiciosModel = require('./models/ComentServicios');
+const FotoServiciosModel = require('./models/FotoServicios');
+const PersonasModel = require('./models/Personas');
+const ServiciosModel = require('./models/Servicios');
+const ServiciosOfreceModel = require('./models/serviciosOfrece');
+
 var conexion = new Sequelize('fastservices','root','12345678',{
     dialect: 'mysql',
 });
 
-var Personas=
-
+const Comentarios = ComentariosModel(conexion,Sequelize);
+const ComentServicios = ComentServiciosModel(conexion,Sequelize);
+const FotoServicios = FotoServiciosModel(conexion,Sequelize);
+const Personas = PersonasModel(conexion,Sequelize);
+const Servicios = ServiciosModel(conexion,Sequelize);
+const ServiciosOfrece = ServiciosOfreceModel(conexion,Sequelize);
 
 conexion.sync().then(()=>{
     console.log('Tablas Creadas exitosamente!')
