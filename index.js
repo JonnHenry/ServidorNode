3de958aaf0d7aa8748fb5951f52b548c64933e95
Jonnathan Campoberde <jonnathanca9601@gmail.com>
2019-01-30 17:50:11 -0500

    Importando todo lo que se necesita del servidor

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var cors = require('cors');
+const bodyParser = require('body-parser')
+const mudulo = require('./sequelize.js')//Se trae todo lo que hay en module.exports del archivo sequelize
+
+const Personas = modulo.Personas;
+const Servicios = modulo.Servicios;
+const Comentarios = modulo.Comentarios;
+const Servicios_Personas = modulo.Servicios_Personas;
+const FotoServicios = modulo.FotoServicios;
+const Comentarios_Servicios = modulo.Comentarios_Servicios;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+var app = express();
+var port = 3000;
+
+
+
+app.listen(port, () => {
+    console.log('Escuchando en el puerto'+port);
+  })
--- sequelize.js
+++ sequelize.js
@@ -20,3 +20,10 @@ const Servicios_Persona = Servicios_PersonaModel(conexion,Sequelize);
 conexion.sync({force: true}).then(()=>{
     console.log('Tablas Creadas exitosamente!')
 });
+
+module.exports.Personas = Personas;
+module.exports.Comentarios = Comentarios;
+module.exports.Servicios = Servicios;
+module.exports.Comentarios_Servicios = Comentarios_Servicios;
+module.exports.FotoServicios = FotoServicios;
+module.exports.Servicios_Persona = Servicios_Persona;
